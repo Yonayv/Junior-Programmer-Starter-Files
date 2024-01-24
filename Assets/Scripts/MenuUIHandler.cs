14ce27b56a5b50db55b6f30f41eb325be5b6ad6d
yonayvfixico <emily.fixico@gctcok.edu>
2024-01-24 13:44:19 -0600

    Data Persistence

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -15,6 +15,7 @@ public class MenuUIHandler : MonoBehaviour
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
+       MainManager.Instance.TeamColor = color;
     }
     
     private void Start()
@@ -22,6 +23,7 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);    
     }
 
     public void StartNew()
@@ -31,10 +33,27 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
-#if UNITY_EDITOR 
+        MainManager.Instance.SaveColor();
+
+
+#if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
         Application.Quit();
 #endif      
     }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
+
+
+
 }
